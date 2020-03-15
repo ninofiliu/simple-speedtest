ff40ac1ad7f2d3c5703710bf60d124fcff4d6c6d
ninofiliu <nino.filiu@gmail.com>
2020-03-16 00:10:19 +0100

    add main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const humanize = require('./humanize');
+
+const url = process.argv[2] || 'http://www.google.com';
+
+const t0 = new Date();
+http.request(new URL(url), (res) => {
+    let bits = 0;
+    let t1;
+    res.on('data', (chunk) => {
+        bits += chunk.length * 8;
+        t1 = new Date();
+        report();
+    });
+    res.on('end', () => {
+        t1 = new Date();
+        console.log('---');
+        report();
+    });
+
+    function report() {
+        const seconds = (t1 - t0) / 1000;
+        const bitsPerSecond = bits / seconds;
+        console.log(`${humanize(bitsPerSecond)}b/s`);
+    }
+}).end();
